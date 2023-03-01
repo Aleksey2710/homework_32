a0936a89e9152144f2ee47d855ab64b5bcf3cda2
Aleksey2710 <Aleksey_2710@mail.ru>
2023-03-01 12:22:58 +0300

    initial commit

---
+++ src/main/java/pro/sky/homework_32/Homework32Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework32Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/homework_32/Homework32ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework32ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
